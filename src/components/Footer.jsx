09eef521d1da6042fe3ed7321c700b593f64100b
LeThanhCong <ltchcmus@gmail.com>
2025-11-03 22:21:01 +0700

    change editer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,7 +48,7 @@ function Footer() {
               }
             }} 
           /> 
-          by Your Name
+          by Le Thanh Cong
         </Typography>
         {/* Copyright text với năm tự động */}
         <Typography 
